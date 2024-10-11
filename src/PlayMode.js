844692abad08f6b6499a9a1c03fc9cab6ad215ef
Vincent Mahnke <vincent@mahn.ke>
2024-10-11 21:04:34 +0200

    automated back-and-forth driving

--- src/PlayMode.js
+++ src/PlayMode.js
@@ -5,7 +5,7 @@ import { Line2 } from 'three/addons/lines/Line2.js';
 import RAPIER from '@dimforge/rapier3d-compat'
 
 export class PlayMode {
-    #playerCube = null;
+    playerCube = null;
     #playerLine = null;
     #attachedCube = null;
     #keys = {};
@@ -18,7 +18,7 @@ export class PlayMode {
         this.#scene = scene;
         this.#world = world;
         this.moveSpeed = 5;
-        this.turnSpeed = 5;
+        this.turnSpeed = 1;
         this.handleKeyDown = this.handleKeyDown.bind(this);
         this.handleKeyUp = this.handleKeyUp.bind(this);
         this.updatePlayerPosition = this.updatePlayerPosition.bind(this);
@@ -42,12 +42,12 @@ export class PlayMode {
 
     attachCube() {
         let direction = new THREE.Vector3();
-        this.#playerCube.cube.getWorldDirection(direction);
+        this.playerCube.cube.getWorldDirection(direction);
         direction.normalize();
         // multiply by 2
         direction = new THREE.Vector3(direction.x * 2, direction.y * 2, direction.z * 2);
 
-        const raycaster = new THREE.Raycaster(this.#playerCube.cube.position, direction);
+        const raycaster = new THREE.Raycaster(this.playerCube.cube.position, direction);
         raycaster.camera = this.#levelLoader.camera;
         raycaster.layers.enableAll();
         const intersects = raycaster.intersectObjects(this.#scene.children, true);
@@ -72,21 +72,21 @@ export class PlayMode {
 
         // Store offset relative to player's position in local space
         const playerPosition = new THREE.Vector3(
-            this.#playerCube.body.translation().x,
-            this.#playerCube.body.translation().y,
-            this.#playerCube.body.translation().z
+            this.playerCube.body.translation().x,
+            this.playerCube.body.translation().y,
+            this.playerCube.body.translation().z
         );
         const attachedPosition = new THREE.Vector3(
             this.#attachedCube.body.translation().x,
             this.#attachedCube.body.translation().y,
             this.#attachedCube.body.translation().z
         );
-        const rapierQuat = this.#playerCube.body.rotation();
+        const rapierQuat = this.playerCube.body.rotation();
         const threeQuat = new THREE.Quaternion(rapierQuat.x, rapierQuat.y, rapierQuat.z, rapierQuat.w);
         const offset = attachedPosition.sub(playerPosition).applyQuaternion(threeQuat.conjugate());
         this.#attachedCube.body.offset = offset;
         // Store the y euler rotation of the player and attached cube
-        const playerEulerInRads = QuaternionUtilities.quaternionToEuler(this.#playerCube.body.rotation());
+        const playerEulerInRads = QuaternionUtilities.quaternionToEuler(this.playerCube.body.rotation());
         const attachedEulerInRads = QuaternionUtilities.quaternionToEuler(this.#attachedCube.body.rotation());
         console.log({offset: this.#attachedCube.body.offset})
         this.#attachedCube.body.playerRotationYInRads = playerEulerInRads.y;
@@ -103,13 +103,13 @@ export class PlayMode {
     }
 
     updatePlayerPosition({delta}) {
-        if (!this.#playerCube) return;
+        if (!this.playerCube) return;
 
         const direction = new THREE.Vector3();
-        this.#playerCube.cube.getWorldDirection(direction);
+        this.playerCube.cube.getWorldDirection(direction);
 
         let newPos = {x: 0, y: 0, z: 0};
-        let newRot = this.#playerCube.body.rotation();
+        let newRot = this.playerCube.body.rotation();
 
         if (this.#keys['w']) {
             newPos.x += direction.x * this.moveSpeed * delta;
@@ -122,7 +122,7 @@ export class PlayMode {
         let rotationThisFrame = null;
         {
             const right = new THREE.Vector3();
-            this.#playerCube.cube.getWorldDirection(right);
+            this.playerCube.cube.getWorldDirection(right);
             right.cross(new THREE.Vector3(0, 1, 0));
             right.normalize();
 
@@ -146,26 +146,26 @@ export class PlayMode {
             }
         }
 
-        this.characterController.computeColliderMovement(this.#playerCube.collider, newPos, null, null, (collider) => {
+        this.characterController.computeColliderMovement(this.playerCube.collider, newPos, null, null, (collider) => {
             return collider.collisionGroups() != attachedCollisionGroup;
         });
         let correctedMovement = this.characterController.computedMovement();
 
-        this.#playerCube.body.setLinvel(new RAPIER.Vector3(correctedMovement.x / delta, correctedMovement.y / delta, correctedMovement.z / delta));
-        this.#playerCube.body.setTranslation(new RAPIER.Vector3(this.#playerCube.body.translation().x, 0.5, this.#playerCube.body.translation().z));
-        this.#playerCube.body.setRotation(newRot);
+        this.playerCube.body.setLinvel(new RAPIER.Vector3(correctedMovement.x / delta, correctedMovement.y / delta, correctedMovement.z / delta));
+        this.playerCube.body.setTranslation(new RAPIER.Vector3(this.playerCube.body.translation().x, 0.5, this.playerCube.body.translation().z));
+        this.playerCube.body.setRotation(newRot);
 
         // Update player line to always shoot from player position + 1 unit in front
         const shootDirection = new THREE.Vector3();
-        this.#playerCube.cube.getWorldDirection(shootDirection);
+        this.playerCube.cube.getWorldDirection(shootDirection);
         shootDirection.normalize();
-        const shootPosition = this.#playerCube.cube.position.clone().add(shootDirection);
+        const shootPosition = this.playerCube.cube.position.clone().add(shootDirection);
         this.#playerLine.geometry.setPositions( [
-            this.#playerCube.cube.position.x, this.#playerCube.cube.position.y, this.#playerCube.cube.position.z,
+            this.playerCube.cube.position.x, this.playerCube.cube.position.y, this.playerCube.cube.position.z,
             shootPosition.x, shootPosition.y, shootPosition.z,
-            this.#playerCube.cube.position.x, this.#playerCube.cube.position.y, this.#playerCube.cube.position.z
+            this.playerCube.cube.position.x, this.playerCube.cube.position.y, this.playerCube.cube.position.z
         ] );
-        this.playerLight.position.set(this.#playerCube.cube.position.x, this.#playerCube.cube.position.y, this.#playerCube.cube.position.z);
+        this.playerLight.position.set(this.playerCube.cube.position.x, this.playerCube.cube.position.y, this.playerCube.cube.position.z);
 
         // Move attached cube with player
         this.updateAttachedCube(rotationThisFrame);
@@ -175,13 +175,13 @@ export class PlayMode {
         if (!this.#attachedCube) return;
 
         const direction = new THREE.Vector3();
-        this.#playerCube.cube.getWorldDirection(direction);
+        this.playerCube.cube.getWorldDirection(direction);
 
-        const offset = this.#attachedCube.body.offset.clone().applyQuaternion(this.#playerCube.body.rotation());
+        const offset = this.#attachedCube.body.offset.clone().applyQuaternion(this.playerCube.body.rotation());
         const newPos = new THREE.Vector3(
-            this.#playerCube.body.translation().x + offset.x,
-            this.#playerCube.body.translation().y + offset.y,
-            this.#playerCube.body.translation().z + offset.z
+            this.playerCube.body.translation().x + offset.x,
+            this.playerCube.body.translation().y + offset.y,
+            this.playerCube.body.translation().z + offset.z
         );
 
         this.#attachedCube.body.setTranslation(newPos);
@@ -212,10 +212,10 @@ export class PlayMode {
         this.playerLight.position.set(x, 1, y);
         this.#scene.add( this.playerLight );
 
-        this.#playerCube = this.#levelLoader.spawnCube(x, y, 1, 1, ObjectTypes.PLAYER);
+        this.playerCube = this.#levelLoader.spawnCube(x, y, 1, 1, ObjectTypes.PLAYER);
         this.characterController = this.#world.createCharacterController(0.01);
         this.characterController.setApplyImpulsesToDynamicBodies(true);
-        this.#playerCube.body.setRotation(QuaternionUtilities.quaternionFromEuler(0, Math.PI, 0))
+        this.playerCube.body.setRotation(QuaternionUtilities.quaternionFromEuler(0, Math.PI, 0))
 
         this.#playerLine = new Line2();
         this.#playerLine.material.color.set( 0xffffff );
@@ -228,13 +228,13 @@ export class PlayMode {
     }
 
     disable() {
-        if (!this.#playerCube) return;
+        if (!this.playerCube) return;
 
         this.#scene.remove(this.#playerLine);
         this.#scene.remove(this.playerLight);
 
-        this.#playerCube.remove();
-        this.#playerCube = null;
+        this.playerCube.remove();
+        this.playerCube = null;
 
         document.removeEventListener('keydown', this.handleKeyDown);
         document.removeEventListener('keyup', this.handleKeyUp);
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import RAPIER from '@dimforge/rapier3d-compat';
 import { EditorMode } from './EditorMode.js';
 import { PlayMode } from './PlayMode.js';
 import { LevelLoader } from './LevelLoader.js';
+import { QuaternionUtilities } from './Utility.js';
 
 await RAPIER.init()
 const gravity = new RAPIER.Vector3(0.0, -9.81, 0.0)
@@ -51,9 +52,10 @@ class UpdateEvent extends Event {
     }
 }
 
+const isPlayModeCheckbox = document.createElement('input');
+const playMode = new PlayMode(levelLoader, scene, world);
+const editorMode = new EditorMode(levelLoader.camera);
 {
-    const playMode = new PlayMode(levelLoader, scene, world);
-    const editorMode = new EditorMode(levelLoader.camera);
 
     const playModeLabel = document.createElement('label');
     playModeLabel.innerText = 'PlayMode';
@@ -62,7 +64,6 @@ class UpdateEvent extends Event {
     playModeLabel.style.right = '30px';
     playModeLabel.style.color = 'white';
 
-    const isPlayModeCheckbox = document.createElement('input');
     isPlayModeCheckbox.type = 'checkbox';
 
     playModeLabel.appendChild(isPlayModeCheckbox);
@@ -87,6 +88,92 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+    let currentKey = null;
+    let state = "ROTATING_LEFT";
+    const automateInput = async () => {
+        if (!isPlayModeCheckbox.checked) {
+            return;
+        }
+
+        if (!playMode.playerCube) {
+            return;
+        }
+
+        const left = -90;
+        const right = 90;
+        const tolerance = 1;
+        const rotQuaternion = playMode.playerCube.body.rotation();
+        const rotEuler = QuaternionUtilities.quaternionToEuler(rotQuaternion).y * 180 / Math.PI;
+
+        if (state === "ROTATING_LEFT") {
+            console.log(`rotEuler: ${rotEuler}, left: ${left}, diff: ${Math.abs(rotEuler - left)}`);
+            if (Math.abs(rotEuler - left) < tolerance) {
+                document.dispatchEvent(new KeyboardEvent('keyup', { key: currentKey }));
+                currentKey = null;
+                state = "MOVING_LEFT";
+                return;
+            }
+
+            if (currentKey) {
+                return;
+            }
+
+            currentKey = 'a';
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: currentKey }));
+        }
+
+        if (state === "MOVING_LEFT") {
+            console.log(`x: ${playMode.playerCube.body.translation().x}`);
+            if (playMode.playerCube.body.translation().x < -3) {
+                document.dispatchEvent(new KeyboardEvent('keyup', { key: currentKey }));
+                currentKey = null;
+                state = "ROTATING_RIGHT";
+                return;
+            }
+
+            if (currentKey) {
+                return;
+            }
+
+            currentKey = 'w';
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: currentKey }));
+        }
+
+        if (state === "ROTATING_RIGHT") {
+            console.log(`rotEuler: ${rotEuler}, left: ${right}, diff: ${Math.abs(rotEuler - right)}`);
+            if (Math.abs(rotEuler - right) < tolerance) {
+                document.dispatchEvent(new KeyboardEvent('keyup', { key: currentKey }));
+                currentKey = null;
+                state = "MOVING_RIGHT";
+                return;
+            }
+
+            if (currentKey) {
+                return;
+            }
+
+            currentKey = 'd';
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: currentKey }));
+        }
+
+        if (state === "MOVING_RIGHT") {
+            console.log(`x: ${playMode.playerCube.body.translation().x}`);
+            if (playMode.playerCube.body.translation().x > 2) {
+                document.dispatchEvent(new KeyboardEvent('keyup', { key: currentKey }));
+                currentKey = null;
+                state = "ROTATING_LEFT";
+                return;
+            }
+
+            if (currentKey) {
+                return;
+            }
+
+            currentKey = 'w';
+            document.dispatchEvent(new KeyboardEvent('keydown', { key: currentKey }));
+        }
+    }
+
 const gameLoop = () => {
     delta = clock.getDelta()
     world.timestep = Math.min(delta, 0.1)
@@ -95,6 +182,8 @@ const gameLoop = () => {
     document.dispatchEvent(readInputEvent);
     const updateEvent = new UpdateEvent(delta);
     document.dispatchEvent(updateEvent);
+
+    automateInput();
 
     world.step();
 
